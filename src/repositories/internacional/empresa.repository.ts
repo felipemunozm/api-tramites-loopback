d5ad1eab7dccb32294d694ec2755682bd9ff5343
Felipe Munoz <felipe.munozm@gmail.com>
2019-05-10 19:39:41 -0400

    a

--- src/repositories/internacional/empresa.repository.ts
+++ src/repositories/internacional/empresa.repository.ts
@@ -12,4 +12,17 @@ export class EmpresaRepository extends DefaultCrudRepository<
   ) {
     super(Empresa, dataSource);
   }
+
+  public obtenerEmpresaByRut(rut: string): Promise<any> {
+    let query = "select a.id, pj.id persona_juridica_id, pj.identificador, pj.razon_social, te.nombre as tipo_empresa,\n" +
+      "de.codigo_comuna, co.nombre as nombre_comuna, de.codigo_region, re.nombre as nombre_region, de.texto, de.telefono_fijo, de.telefono_movil, de.email,\n" +
+      "pn.id as id_representante_legal, pn.identificador identificador_representante_legal, pn.nombre_completo nombre_representante_legal from empresa a\n" +
+      "right join persona_juridica pj on a.persona_juridica_id = pj.id\n" +
+      "left join domicilio_empresa de on a.id = de.empresa_id\n" +
+      "left join tipo_empresa te on a.tipo_empresa_id = te.id\n" +
+      "left join comuna co on de.codigo_comuna = co.codigo\n" +
+      "left join region re on de.codigo_region = re.codigo\n" +
+      "left join persona_natural pn on pj.representante_legal_id = pn.id where pj.identificador = $1";
+    return this.dataSource.execute(query, [rut]);
+  }
 }
