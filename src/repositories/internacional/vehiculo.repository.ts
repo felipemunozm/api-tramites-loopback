ca3e1fd89b0b4827682a1b051096c521424c5d24
Claudio Cofre Soto <ccofre@mtt.cl>
2019-05-16 13:07:58 -0400

    nueva query

--- src/repositories/internacional/vehiculo.repository.ts
+++ src/repositories/internacional/vehiculo.repository.ts
@@ -19,4 +19,9 @@ export class VehiculoRepository extends DefaultCrudRepository<
       "inner join vehiculo v on v.id = sv.vehiculo_id where a.permiso_id =$1";
     return this.dataSource.execute(query, [id]);
   }
+  public crearVehiculo(vehiculo: any): Promise<any> {
+    let query: string = "insert into vehiculo (version, cantidad_ejes, identificador, tipo, cantidad_toneladas_carga, tipo_id_id, modelo, marca, ppu, anno_fabricacion, carroceria, nombre_propietario)'
+      + ' values (0, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) returning id";
+    return this.dataSource.execute(query, vehiculo);
+  }
 }
