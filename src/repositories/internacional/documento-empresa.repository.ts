7d738921dd044860a27c63c9fd0ae9736292895d
Claudio Cofre Soto <ccofre@mtt.cl>
2019-05-16 12:23:30 -0400

    Nuevo Proceso

--- src/repositories/internacional/documento-empresa.repository.ts
+++ src/repositories/internacional/documento-empresa.repository.ts
@@ -16,4 +16,8 @@ export class DocumentoEmpresaRepository extends DefaultCrudRepository<
     let query: string = "insert into documento_empresa (version, tipo_id, empresa_id, url, hashing, algoritmo) values (0, $1, $2, $3, $4, $5) returning id";
     return this.dataSource.execute(query, [tipoDocumentoId, empresaId, url, '', '']);
   }
+  public obtenerDocumentosEmpresaById(id: string): Promise<any> {
+    let query: string = "select td.nombre, td.codigo, doc.url from documento_empresa as doc join tipo_documento td on doc.tipo_id = td.id where empresa_id = $1";
+    return this.dataSource.execute(query, [id]);
+  }
 }
