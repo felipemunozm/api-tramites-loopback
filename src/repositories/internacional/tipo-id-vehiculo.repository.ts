e195748060c0648e75fd9e3d9c456c33c6d670ca
Claudio Cofre Soto <ccofre@mtt.cl>
2019-05-16 12:32:34 -0400

    Modificación de query

--- src/repositories/internacional/tipo-id-vehiculo.repository.ts
+++ src/repositories/internacional/tipo-id-vehiculo.repository.ts
@@ -16,7 +16,7 @@ export class TipoIdVehiculoRepository extends DefaultCrudRepository<
     let query: string = "select id, codigo, pais_id, nombre from tipo_id_vehiculo";
     return this.dataSource.execute(query);
   }
-  public obtenerTipoIdentificadorVehiculoByCodigo(codigo: string): Promise<any> {
+  public obtenerTipoIdentificadorVehiculoByCodigo(codigo: any): Promise<any> {
     let query: string = "select id, codigo, pais_id, nombre from tipo_id_vehiculo where codigo = $1";
     return this.dataSource.execute(query, [codigo]);
   }
