938706fe74cdd44d8bc34516a37fdc674f303486
Claudio Cofre Soto <ccofre@mtt.cl>
2019-05-16 12:59:52 -0400

    Nueva query

--- src/repositories/internacional/direccion-persona-natural.repository.ts
+++ src/repositories/internacional/direccion-persona-natural.repository.ts
@@ -12,11 +12,15 @@ export class DireccionPersonaNaturalRepository extends DefaultCrudRepository<
   ) {
     super(DireccionPersonaNatural, dataSource);
   }
-  public obtenerDireccionByPersonaId(id: string): Promise<any> {
+  public obtenerDireccionByPersonaId(id: any): Promise<any> {
     let query = "select dpa.codigo_region, dpa.codigo_comuna, dpa.texto, co.nombre as nombre_comuna, re.nombre as nombre_region from direccion_persona_natural dpa\n" +
       "left join comuna co on co.codigo = dpa.codigo_comuna\n" +
       "left join region re on re.codigo = dpa.codigo_region\n" +
       "where persona_id = $1";
     return this.dataSource.execute(query, [id]);
+  }
+  public crearDireccionPersonaNatural(direccion: any): Promise<any> {
+    let query: string = "insert into direccion_persona_natural (version, codigo_region, codigo_comuna, tipo, texto, persona_id) values (0, $1, $2, $3, $4, $5) returning id";
+    return this.dataSource.execute(query, [direccion])
   }
 }
