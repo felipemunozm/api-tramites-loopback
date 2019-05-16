7c330dec4935959c63821f2f8420b53afd91ad51
Felipe Antonio Muñoz Montecino <fmunoz@mtt.cl>
2019-05-16 15:21:55 -0400

    otro metodo

--- src/repositories/tramites/tramite.repository.ts
+++ src/repositories/tramites/tramite.repository.ts
@@ -16,4 +16,9 @@ export class TramiteRepository extends DefaultCrudRepository<
     let query: string = "select id, identificador_intermediario, analista_aprobacion_id, solicitud_id, metadata, codigo, fecha_hora_creacion, tipo_tramite_id, intermediario_id from tramite where solicitud_id = $1";
     return this.dataSource.execute(query, [solicitudId]);
   }
+  public crearTramite(tramite: any): Promise<any> {
+    let query = 'insert into tramite (version, identificador_intermediario, analista_aprobacion_id, solicitud_id, metadata, codigo, fecha_hora_creacion, tipo_tramite_id, intermediario_id) '
+      + 'values (0, $1, $2, $3, $4, $5, $6, $7, $8) returning id';
+    return this.dataSource.execute(query, [tramite.identificadorIntermediario, tramite.analistaId, tramite.solicitudId, tramite.metadata, tramite.codigo, tramite.fechaHoraCreacion, tramite.tipoTramiteId, tramite.intermediarioId]);
+  }
 }
