21facbf366bbedd7fcf2892f709d7d21b8959cbf
Claudio Cofre Soto <ccofre@mtt.cl>
2019-05-16 12:29:13 -0400

    Nuevo proceso

--- src/repositories/internacional/persona-natural.repository.ts
+++ src/repositories/internacional/persona-natural.repository.ts
@@ -25,4 +25,11 @@ export class PersonaNaturalRepository extends DefaultCrudRepository<
     let query = "insert into direccion_persona_natural (version, codigo_region, codigo_comuna, tipo, texto, persona_id) values (0, $1, $2, $3, $4, $5) returning id";
     return this.dataSource.execute(query, [direccion.codigo_region, direccion.codigo_comuna, direccion.tipo, direccion.texto, direccion.persona_id]);
   }
+  public obtenerDireccionByPersonaId(id: string): Promise<any> {
+    let query = "select dpa.codigo_region, dpa.codigo_comuna, dpa.texto, co.nombre as nombre_comuna, re.nombre as nombre_region from direccion_persona_natural dpa\n" +
+      "left join comuna co on co.codigo = dpa.codigo_comuna\n" +
+      "left join region re on re.codigo = dpa.codigo_region\n" +
+      "where persona_id = $1";
+    return this.dataSource.execute(query, [id]);
+  }
 }
