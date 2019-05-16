0fc4a4b9c518a404dceace78a7a85eacca6cc845
Claudio Cofre Soto <ccofre@mtt.cl>
2019-05-16 12:43:22 -0400

    Nueva query

--- src/repositories/internacional/permiso-sujeto-vehiculo.repository.ts
+++ src/repositories/internacional/permiso-sujeto-vehiculo.repository.ts
@@ -12,4 +12,11 @@ export class PermisoSujetoVehiculoRepository extends DefaultCrudRepository<
   ) {
     super(PermisoSujetoVehiculo, dataSource);
   }
+  public obtenerVehiculosByPermisoId(id: any): Promise<any> {
+    let query: string = "select distinct v.ppu, a.habilitado, v.cantidad_ejes, v.tipo, v.cantidad_toneladas_carga, v.modelo, v.marca, v.chasis, v.num_motor, \n" +
+      "v.anno_fabricacion, v.carroceria, v.nombre_propietario\n" +
+      "from permiso_sujeto_vehiculo a inner join sujeto_vehiculo sv on sv.id = a.sujeto_vehiculo_id\n" +
+      "inner join vehiculo v on v.id = sv.vehiculo_id where a.permiso_id =$1";
+    return this.dataSource.execute(query, [id]);
+  }
 }
