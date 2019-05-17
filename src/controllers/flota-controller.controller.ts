92bcc2e3efcf2938aca30eb36b03aef1257ac831
Felipe Antonio Muñoz Montecino <fmunoz@mtt.cl>
2019-05-17 11:21:26 -0400

    404

--- src/controllers/flota-controller.controller.ts
+++ src/controllers/flota-controller.controller.ts
@@ -25,7 +25,7 @@ export class FlotaControllerController {
       let params: any = q;
       if (!params || !params.rutSujeto || !params.ppus || params.ppus.length === 0) {
         // throw { error: { statusCode: 502, message: 'Parámetros incorrectos' } };
-        return HttpErrors(402, 'Parámetros incorrectoss');
+        return HttpErrors('Parámetros incorrectoss', 404);
       }
       let newtipArr = [], vehiculosTipo2 = [], vehiculosRechazados3: any[] = [], promisesPpus: any[] = [], ppusProcesadas: any[] = [], promisesPrt: any[] = [], vehiculosRechazados: any[] = [], vehiculosRechazados2: any[] = [], vehiculosValidados: any[] = [], vehiculosValidadosPorTipo: any[] = [], motivosRechazos: any[] = [], totalCarga = 0, vehiculosDocsLeasing: any[] = [], vehiculosDocsRevision: any[] = [], vehiculosValidadosPar: any[] = [], contadorRechazos: any[] = [], contadorParcial: any = [], Noexiste: any[] = [], ppuRech: any[] = [], docLeasing = false, docRevision = false, existe: any;
       params.ppus.forEach((ppu: any[]) => {
