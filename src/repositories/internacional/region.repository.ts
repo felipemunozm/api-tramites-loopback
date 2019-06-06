bc077f0f7907ecab6c728ae755264e008da829a5
Felipe Antonio Muñoz Montecino <fmunoz@mtt.cl>
2019-06-06 14:37:14 -0400

    repositorio regiones update

--- src/repositories/internacional/region.repository.ts
+++ src/repositories/internacional/region.repository.ts
@@ -16,4 +16,8 @@ export class RegionRepository extends DefaultCrudRepository<
     let query: string = "select id, codigo, nombre from region order by id asc";
     return this.dataSource.execute(query);
   }
+  public obtenerRegionesByCodigo(codigo: any): Promise<any> {
+    let query: string = "select id, codigo, nombre from region where codigo = $1 order by id asc"
+    return this.dataSource.execute(query, [codigo]);
+  }
 }
