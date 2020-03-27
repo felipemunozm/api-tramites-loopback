421bb18d991a8f4a1163ac08a669436cfd356347
COFRÉ SOTO CLAUDIO ALBERTO <ccofre@mtt.cl>
2020-03-27 18:13:25 -0300

    Se agrega campo codigo_empresa

--- src/controllers/personas-controller.controller.ts
+++ src/controllers/personas-controller.controller.ts
@@ -170,6 +170,7 @@ export class PersonasControllerController {
       if (permiso == undefined || permiso.tipo_estado_permiso_id == null) {
         resp.empresa = {}
         resp.empresa.razonSocial = empresa.razon_social
+        resp.empresa.codigo_empresa = empresa.codigo_empresa
         resp.empresa.direccion = {
           codigo_comuna: empresa.codigo_comuna,
           codigo_region: empresa.codigo_region,
@@ -199,6 +200,7 @@ export class PersonasControllerController {
           resp.descripcionResultado = 'Empresa Registrada, Usuario Autorizado, Sin permiso vigente'
           resp.empresa = {}
           resp.empresa.razonSocial = empresa.razon_social
+          resp.empresa.codigo_empresa = empresa.codigo_empresa
           resp.empresa.direccion = {
             codigo_comuna: empresa.codigo_comuna,
             codigo_region: empresa.codigo_region,
@@ -219,6 +221,7 @@ export class PersonasControllerController {
       resp.empresa = {}
       resp.empresa.rut = empresa.identificador
       resp.empresa.razonSocial = empresa.razon_social
+      resp.empresa.codigo_empresa = empresa.codigo_empresa
       resp.empresa.tipo = empresa.tipo_empresa
       resp.empresa.direccion = {
         codigo_comuna: empresa.codigo_comuna,
